7d1a0b0625200eaeb0091b429e4b00ac9878e315
Jimmy Lam <dev.jimmylam@gmail.com>
2020-07-23 12:57:26 -0700

    created sound variable

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Audio } from 'expo-av';
+
+
+const listBackgroundColors = {
+  1: "#3498DB",
+  2: "#E8290B",
+  3: "#AE1438",
+  4: "#2475B0",
+  5: "#30336B",
+  6: "#0A3D62",
+  7: "#26ae60",
+  8: "#10A881",
+  9: "#218F76",
+  10: "#DFAF2B"
+};
+
+const soundList = {
+  one: require("./assets/one.wav"),
+  two: require("./assets/two.wav"),
+  three: require("./assets/three.wav"),
+  four: require("./assets/four.wav"),
+  five: require("./assets/five.wav"),
+  six: require("./assets/six.wav"),
+  seven: require("./assets/seven.wav"),
+  eight: require("./assets/eight.wav"),
+  nine: require("./assets/nine.wav"),
+  ten: require("./assets/ten.wav")
+};
 
 export default function App() {
+  //TODO: function to play sound
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Spanish Number App</Text>
       <StatusBar style="auto" />
     </View>
   );
